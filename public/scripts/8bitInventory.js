c65204af90ffab4b220161d90307047559ae4053
Deforis Nash <defhnash@gmail.com>
2017-01-01 18:19:35 -0600

    finished and styled

--- public/scripts/8bitInventory.js
+++ public/scripts/8bitInventory.js
@@ -16,6 +16,14 @@ $( document ).ready( function(){
     }; // end testObject
     console.log( 'adding:', newItem );
     ////// TODO: add ajax call to addItem route to add this item to the table
+    $.ajax({
+      type: 'POST',
+      url:'/addItem',
+      data: newItem,
+      success:function(data){
+        console.log("Post got", data);
+      }
+    });
     // add to items array
     items.push( newItem );
   }; // end addObject
@@ -24,27 +32,61 @@ $( document ).ready( function(){
     console.log( 'in findObject. Looking for:', colorCheck, sizeCheck );
     // array of matches
     var matches = [];
+    $('#searchDiv').html(' ');
     for ( var i = 0; i < items.length; i++ ) {
-      if( items[i].color == colorCheck && items[i].size == sizeCheck ){
+      if( items[i].color == colorCheck || items[i].size == sizeCheck ){
         // match, add to array
         matches.push( items[i] );
+        $('#searchDiv').append("<p class='searchRes'>" + items[i].item + "</p>");
       } // end if
     } // end for
     console.log( 'matches:', matches );
-    ////// TODO: display matches
+    if (matches.length === 0) {
+      $('#searchDiv').append('None found...');
+    }
+
   }; // end findObject
 
   var getObjects = function(){
     console.log( 'in getObjects');
     // populate the items array
     ////// TODO: replace the stuff in this function with getting items from the database ////////
+    $.ajax({
+      type: 'GET',
+      url: '/getInventory',
+      success:function(data){
+        console.log('Got', data);
+        items = [];
+        for (var i = 0; i < data.length; i++) {
+          items.push(data[i]);
+        }
+        console.log('Heres whsats in', items);
+        $('#output').html('');
+        for (var x = 0; x < items.length; x++) {
+          $('#output').append("<p class='result'>" + items[x].size + ' ' + items[x].color + ' ' +  items[x].item + "</p>");
+        }
+      }
+    });
+
     ////// hint: make a get call to the getInventory and use it's response data to fill the items array ////////
   }; // end getObjects
 
   // get objects when doc is ready
   getObjects();
   // the below are tests to show what is returned when running findObject
-  addObject( 'blue', 'blueberry', 'small' )
+  // addObject( 'blue', 'blueberry', 'small' );
   findObject( 'blue', 'small' );
   findObject( 'blue', 'large' );
+  $('#postButton').on('click', function(){
+    addObject($('#color').val(),$('#item').val(), $('#size').val());
+    getObjects();
+    $('#color').val('');
+    $('#item').val('');
+    $('#size').val('');
+
+  });
+  $('#searchButton').on('click', function(){
+    findObject($('#color').val(), $('#size').val());
+
+  });
 }); // end doc ready
--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
 var urlEncodedParser = bodyParser.urlencoded( { extended: false } );
 var port = process.env.PORT || 3003;
+var connectionString = 'postgres://localhost:5432/inventory';
+var pg = require('pg');
 
 // spin up server
 app.listen( port, function(){
@@ -19,14 +21,41 @@ app.get( '/', function( req, res ){
 // add new objects to the inventory
 app.post( '/addItem', urlEncodedParser, function( req, res ){
   console.log( 'addItem route hit:', req.body );
+  pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+          console.log("something borked in the post");
+        } else {
+          console.log("posting");
+          client.query('INSERT INTO items (item, size, color) values ( $1, $2, $3 )', [req.body.name, req.body.size, req.body.color]);
+            res.send("Done!");
+        }
   // add the item from req.body to the table
+}); 
 }); // end addItem route
 
 // get all objects in the inventory
 app.get( '/getInventory', function( req, res ){
   console.log( 'getInventory route hit' );
+  var x = 'Hello!';
+  pg.connect(connectionString, function(err, client, done) {
+          if (err) {
+              console.log("Broked");
+          } else {
+            console.log("Success!");
+              var query = client.query("SELECT * FROM items");
+              var array = [];
+              query.on('row', function(row) {
+                  array.push(row);
+              });//end on row
+              query.on('end', function() {
+                  done();
+                  console.log(array);
+                  res.send(array);
+              });//end on end
+          }
+        });
   // get all items in the table and return them to client
-}); // end addItem route
+}); // end get Inventory
 
 // static folder
 app.use( express.static( 'public' ) );
